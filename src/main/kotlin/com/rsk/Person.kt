13840ecb2abe3d18867a0d6c5a63951b024bb711
Txny <irakozeyves9@gmail.com>
2022-07-22 15:48:18 +0200

    Classes, Constructors and Default Parameters

--- src/main/kotlin/com/rsk/Person.kt
+++ src/main/kotlin/com/rsk/Person.kt
@@ -9,14 +9,19 @@ interface Signatory{
 // passing attributes in the class definition, exempts us from having to create a constructor, seeing as it is created for us by default.
 // val is for values that won't change. like constants. var is for values that may change. for example in main, I could change age, but changing name resulted in an error
 // adding the open keyword on class makes it inheritable. normally, Kotlin makes all classes final, which makes it impossible to inherit from them
-open class Person(val name:String, var age:Int) : Signatory{
-    var isMarried = false
+open class Person(val name:String, var age:Int, var isMarried:Boolean = false) : Signatory{
+//    var isMarried = false
 
 //    the constructor keyword is used to create secondary constructors.
-    constructor(name: String, age: Int, isMarried: Boolean): this(name, age){
-        this.isMarried = isMarried
-    }
+//    However, the preferred approach is to use a feature called default parameters
+//    So, rather than having to use a secondary constructor we can attach default parameters to properties
+//    then our code changes from below with a secondary constructor, to above in the class definition where we added a third parameter, with a default value
+//    constructor(name: String, age: Int, isMarried: Boolean): this(name, age){
+//    the "this" keyword calls the secondary constructor through the primary constructor
+//        this.isMarried = isMarried
+//    }
 //    init runs right after an object is constructed. reminds me of python. Finally, something I recognize. Guess it isn't that hard
+//    init in kotlin is used to do some more initialization that we couldn't do with the primary constructor. this block is always execute after the constructor
 //    init {
 //        if (name == "Paul" && age < 27) throw Exception("Invalid age")
 //    }
@@ -28,7 +33,7 @@ open class Person(val name:String, var age:Int) : Signatory{
 // when inheriting from a class, we can't put the types(val or var) of the values as this would override the types from the superclass. (Don't know why it's a bad thing)
 class Student(name:String, age:Int) : Person(name, age){}
 fun main(args: Array<String>){
-    val p = Person("Paul", 21)
+    val p = Person("Paul", 21, true)
     p.sign()
     p.age = 26
     p.sign()
