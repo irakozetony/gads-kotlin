3dc4eaca9e966008396e60dc08b8b629a2c0d54d
Txny <irakozeyves9@gmail.com>
2022-07-24 00:01:35 +0200

    Mapping

--- src/main/kotlin/Program.kt
+++ src/main/kotlin/Program.kt
@@ -1,17 +1,27 @@
 import com.rsk.Providers
 
 fun main(args: Array<String>){
+
     val providers = Providers()
+    val details = providers.getAllProviders()
 
-    val allProviders = providers.getProviders()
+    details.forEach(::println)
+//    this is a higher order function. I don't know what that means yet.
+//    other higher order functions include: map
+//    map changes one collection into another
+//    getAllProviders{
+//        key, value -> println("\t---$key: $value")
+//    }
+}
 
+fun getAllProviders(fn: (String, String) -> Unit){
+    val allProviders = Providers.getProviders()
     val it = allProviders.iterator()
 
     while (it.hasNext()){
         val provider = it.next()
         println(provider.name)
-
-        provider.forEach { key, value -> println("\t$key: $value") }
+        provider.forEach { key, value -> fn(key.toString(), value.toString()) }
     }
 }
 
--- src/main/kotlin/com/rsk/Providers.kt
+++ src/main/kotlin/com/rsk/Providers.kt
@@ -3,10 +3,22 @@ package com.rsk
 import java.security.Provider
 import java.security.Security
 
+data class ProviderDetails(val providerName: String, val name:String)
 class Providers {
     fun getProviders(): List<Provider>{
         val providers = Security.getProviders()
         val listOfProviders : List<Provider> = providers.asList()
+        return listOfProviders
+    }
+
+    fun getAllProviders(): List<ProviderDetails>{
+        val providers = Security.getProviders()
+        val listOfProviders = mutableListOf<ProviderDetails>()
+        providers.forEach {
+            provider ->
+            val providerDetail = provider.entries.map{ ProviderDetails(provider.name, it.key.toString())}
+            listOfProviders += providerDetail
+        }
         return listOfProviders
     }
 
