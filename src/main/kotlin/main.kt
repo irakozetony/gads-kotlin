269eebdf484b9b2fa09a870e01423d18f6eca3cb
Txny <irakozeyves9@gmail.com>
2022-07-25 17:00:37 +0200

    extension functions

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,6 +3,12 @@
 fun main(args: Array<String>){
     display("I'm alien")
     println(maximum(1, 2))
+    log(message = "Hello world!", logLevel = 1)
+
+    val text = "With    multiple   \t  whitespace"
+    println(replaceMultipleWhiteSpace(text))
+//    calling extension functio
+    println(text.replaceMultipleWhiteSpaceEx())
 
 }
 fun display(message: String): Boolean{
@@ -11,3 +17,20 @@ fun display(message: String): Boolean{
 }
 
 fun maximum(a: Int, b: Int): Int = if (a > b) a else b
+
+fun log(message: String, logLevel: Int = 2){
+    println(message + logLevel)
+}
+
+
+// normal function
+fun replaceMultipleWhiteSpace(value: String) : String{
+    var regex = Regex("\\s+")
+    return regex.replace(value, " ")
+}
+
+// extension function
+fun String.replaceMultipleWhiteSpaceEx() : String{
+    var regex = Regex("\\s+")
+    return regex.replace(this, " ")
+}
