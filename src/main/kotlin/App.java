fb5520a4ad4aec1c49dc6859f8017c25623433ab
Txny <irakozeyves9@gmail.com>
2022-07-25 12:52:36 +0200

    day-five complete

---
+++ src/main/kotlin/App.java
@@ -0,0 +1,8 @@
+import com.rsk.*;
+
+public class App {
+    public static void main(String[] args) {
+        Person p = new Person("Tony");
+        p.display();
+    }
+}
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,101 +1,13 @@
-import com.rsk.Person
-import java.io.FileReader
-import java.io.IOException
-import java.util.TreeMap
+// functional programming
 
 fun main(args: Array<String>){
-//    println("Hello world")
-//    val q:Question? = Question()
-//
-//    q?.answer = "41"
-////    q.question = "kjahlkshdlk"
-//    q?.display()
-//    println("The answer to the question '${q?.question}' is ${q?.answer}")
-//
-//    val message:String =
-//        if (q?.answer == q?.correctAnswer){
-//            "You're correct"
-//        }
-//        else{
-//            "Try again"
-//        }
-//    println(message)
-//    q?.printResult()
-//
-//    val number:Int? = try{
-//        Integer.parseInt(q?.answer)
-//    }
-//    catch(e:NumberFormatException){
-//        null
-//    }
-//    println("Number is $number")
-//
-//    for(i in 10 until 100 step 11){
-//        println(i)
-//    }
-//
-//    var ages = TreeMap<String, Int>()
-//    ages["Kevin"] = 55
-//    ages["Sam"] = 24
-//    ages["Alex"] = 24
-//    ages["Harry"] = 26
-//
-//    for ((name, age) in ages){
-//        println("$name is $age")
-//    }
-//
-//    var numbers = listOf(1,2,3,4,5,6,7,8,9,10)
-//
-//    for(i in numbers){
-//        println(i)
-//    }
-//
-//    for ((index, element) in numbers.withIndex()){
-//        println("$element at $index")
-//    }
+    display("I'm alien")
+    println(maximum(1, 2))
 
-//    var reader = FileReader("filename")
-//
-//    try{
-//        reader.read()
-//    }
-//    catch (e:IOException){
-//
-//    }
-//    finally {
-//
-//    }
+}
+fun display(message: String): Boolean{
+    println("You said $message")
+    return true
 }
 
-//fun printName(name: String){
-//    println(name)
-//}
-
-
-class Question{
-////    ? means answer could be null
-//    var answer:String? = ""
-//    val question:String = "What is the answer to life, the universe and everything?"
-//    val correctAnswer = "42"
-//
-//    fun display(){
-//        println("You said $answer")
-//    }
-//
-//    fun printResult(){
-//        when (answer) {
-//            correctAnswer -> {
-//                println("You were correct")
-//            }
-//            "50" ->{
-//                println("Fifty!")
-//            }
-//            "41" ->{
-//                println("Forty-One")
-//            }
-//            else -> {
-//                println("Try again")
-//            }
-//        }
-//    }
-}
+fun maximum(a: Int, b: Int): Int = if (a > b) a else b
