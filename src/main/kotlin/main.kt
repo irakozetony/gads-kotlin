81cf0a630f8aa1a6ae9b5b97830b1e83dd6d42e1
Txny <irakozeyves9@gmail.com>
2022-07-26 11:43:45 +0200

    infix functions

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -10,6 +10,16 @@ fun main(args: Array<String>){
 //    calling extension functio
     println(text.replaceMultipleWhiteSpaceEx())
 
+    val h1 = Header("H1")
+    val h2 = Header("H2")
+//    normal function
+    val h3 = h1.plus(h2)
+//    infinix function
+    val h4 = h1 + h2
+
+    println(h3.name)
+    println(h4.name)
+
 }
 fun display(message: String): Boolean{
     println("You said $message")
@@ -33,4 +43,11 @@ fun replaceMultipleWhiteSpace(value: String) : String{
 fun String.replaceMultipleWhiteSpaceEx() : String{
     var regex = Regex("\\s+")
     return regex.replace(this, " ")
+}
+
+class Header(var name: String){}
+
+//we can make a function infix. Don't know how it's different from normal functions though
+operator infix fun Header.plus(other: Header):Header{
+    return Header(this.name + other.name)
 }
